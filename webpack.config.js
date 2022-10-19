30cb99696b9e4dae9c2aa6904133a0f2a34ec6d7
tim <tim@timschupp.de>
2022-10-19 19:28:09 +0200

    fix small typo

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ var config = function(env) {
     return {
     context: __dirname,
     entry: {
-        'staticfiles': './src/index.tsx',
+        'staticfiles': './src/index.ts',
     },
 
     resolve: {
